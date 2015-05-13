7ddb428842defd9320ee5117b3bc201f219bb56f
Jose Manuel <jmgcrespo@gmail.com>
2015-05-13 17:53:53 +0100

    Completed blog and post spec. Created Tag and PostTag class and added associations

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -1,22 +1,32 @@
 require_relative 'helper'
+require "byebug"
 
 describe Blog do
-  it 'has many posts through categories' do
-    blog = Blog.create!(name: 'BenBlog')
-    author = blog.authors.create!(name: 'Ben Lovell')
-    category = blog.categories.create!(name: 'Motorsports')
+
+	before do
+    @blog = Blog.create!(name: 'BenBlog')
+    @author = @blog.authors.create!(name: 'Ben Lovell')
+    @category = @blog.categories.create!(name: 'Motorsports')
 
     Post.create!(
       title: 'Formula 1',
       content: 'My first post',
-      author: author,
-      category: category
+      author: @author,
+      category: @category
     )
 
-    blog.posts.empty?.must_equal(false)
+	end
+
+  it 'has many posts through categories' do
+    @blog.posts.empty?.must_equal(false)
   end
 
-  it 'has many authors'
-
-  it 'has many categories'
+  it 'has many authors' do
+    author2 = @blog.authors.create!(name: 'Manu')	
+    @blog.authors.length.must_equal(2)
+  end
+  it 'has many categories' do
+  	@blog.categories.create!(name: 'Sailing')
+  	@blog.categories.count.must_equal(2)
+	end
 end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,13 +1,41 @@
 require_relative 'helper'
+require "byebug"
 
 describe Post do
-  it 'belongs to an author'
 
-  it 'has many comments'
+		before do
+    @blog = Blog.create!(name: 'BenBlog')
+    @author = @blog.authors.create!(name: 'Ben Lovell')
+    @category = @blog.categories.create!(name: 'Motorsports')
 
-  it 'belongs to a category'
+    @post=Post.create!(
+      title: 'Formula 1',
+      content: 'My first post',
+      author: @author,
+      category: @category
+    )
+    @comment1= @post.comments.create!(name: "Manu", content: "Congratulations!")
+    @comment2= @post.comments.create!(name: "Anonymous", content: "I couldn´t agree more")
+    @tag1 = @post.tags.create!(name: "speed")
+    @tag2 = @post.tags.create!(name: "noise")
+	end
+  it 'belongs to an author' do
+  	@author.posts.include?(@post)
+  end
 
-  it 'belongs to a blog through a category'
+  it 'has many comments' do
+  	@post.comments.length.must_equal(2)
+  end
 
-  it 'has many tags through posttags'
+  it 'belongs to a category' do
+  	@category.posts.include?(@post)
+  end
+
+  it 'belongs to a blog through a category' do
+  	@blog.posts.include?(@post)	
+  end
+
+  it 'has many tags through posttags' do
+	skip  	
+  end
 end
