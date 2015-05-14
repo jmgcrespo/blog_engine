472dbfd39b98a4f0c09dc1b278d2fb77e08da884
Ben Lovell <benjamin.lovell@gmail.com>
2015-05-14 16:51:56 +0100

    Added faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
+require 'faker'
+
 require_relative '../models/blog'
 require_relative '../models/author'
 require_relative '../models/category'
 
-blog = Blog.find_or_create_by!(name: 'Ben Lovell Blog')
+blog = Blog.find_or_create_by!(name: Faker::Name.name)
 
-blog.authors.find_or_create_by!(name: 'Ben Lovell', twitter_handle: '@benlovell')
+blog.authors.find_or_create_by!(
+  name: 'Ben Lovell',
+  twitter_handle: Faker::Hacker.adjective
+)
 
-blog.categories.find_or_create_by!(name: 'Motorsports')
-blog.categories.find_or_create_by!(name: 'Computers')
+blog.categories.find_or_create_by!(name: Faker::Commerce.department)
+blog.categories.find_or_create_by!(name: Faker::Commerce.department)
