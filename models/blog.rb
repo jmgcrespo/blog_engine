72dc41531360dd8a5d4c91784afeee50bd092f96
Ben Lovell <benjamin.lovell@gmail.com>
2015-05-15 10:21:05 +0100

    Adds validations for blog

--- models/blog.rb
+++ models/blog.rb
@@ -2,4 +2,8 @@ class Blog < ActiveRecord::Base
   has_many :authors
   has_many :categories
   has_many :posts, through: :categories
+
+  validates :name,
+    presence: true,
+    uniqueness: true
 end
